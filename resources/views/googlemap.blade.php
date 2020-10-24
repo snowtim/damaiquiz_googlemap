22638cbd2e9cbe10d10a635e11c4b3b238974fcf
Your Name <you@example.com>
2020-10-25 04:07:38 +0800

    Googlemap,you can use to search location(get ver)

--- resources/views/googlemap.blade.php
+++ resources/views/googlemap.blade.php
@@ -16,19 +16,16 @@
 	</style>
 
 	<!-- SCRIPT -->
-	<!--script src="https://code.jquery.com/jquery-3.5.1.js"
-  integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc="
-  crossorigin="anonymous"></script-->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </head>
 
 <body>
 	<h1>Google Map Demo</h1>
 
-	<form class="box" method="GET" action="/getaddress">
+	<form class="box" method="GET">
 		<div class="address">
 			<span style="font-size: 0.7em">請選擇縣巿:</span>
-			<select id="city" name="cityid" required>	
+			<select id="city" name="cityid">	
 					<option>Select</option>
 				@foreach($cities as $city)
 					<option value="{{ $city->id }}">{{ $city->city }}</option>
@@ -36,18 +33,18 @@
 			</select>
 
 			<span style="font-size: 0.7em">請選擇鄉鎮巿區:</span>
-			<select id="area" name="areafilename" required>
+			<select id="area" name="areafilename">
 					<option>Select</option>
 			</select>
 
 			<span style="font-size: 0.7em">請選擇路(街):</span>
-			<select id="road" name="road" required>
+			<select id="road" name="road">
 					<option>Select</option>
 			</select>
 		</div>
 
 		<div class="address_la">
-			<input id="land" type="text" name="lane" style="width: 50px">
+			<input id="lane" type="text" name="lane" style="width: 50px">
 			<label style="font-size: 0.7em">巷</label>
 			<input id="alley" type="text" name="alley" style="width: 50px">
 			<label style="font-size: 0.7em">弄</label>
@@ -56,8 +53,8 @@
 			<input id="floor" type="text" name="floor" style="width: 50px">
 			<label style="font-size: 0.7em">樓</label>
 
-		<!-- Using for test Google Map API -->
-		<!--input type="text" class="address" name="address"-->
+			<!-- Using for test Google Map API -->
+			<!--input type="text" class="address" name="address"-->
 		</div>
 
 		<div class="other_info">
@@ -65,26 +62,26 @@
 			<input id="info" type="text" name="info">
 		</div>
 
-			<button class="submit">search</button>
+			<input id="submit" type="button" value="search">
 	</form>	
 
 	<div id="map"></div>
 
 	<!-- Google Map API -->
-	<!--script>
+	<script>
 		var map, geocoder;
 
 		function initMap() {
 
-			//geocoder = new google.maps.Geocoder();
+			geocoder = new google.maps.Geocoder();
 			map = new google.maps.Map(document.getElementById('map'), {
-				center: {lat: 24.001, lng: 120.885},
-				zoom: 10
+				center: {lat: 25.047702, lng: 121.5173735},
+				zoom: 15
 			});
 
 			$('#submit').click(function() {
 				var cityid = $('#city').val();
-				var addressfilename = $('#area').val();
+				var areafilename = $('#area').val();
 				var road = $('#road').val();
 				var lane = $('#lane').val();
 				var alley = $('#alley').val();
@@ -107,56 +104,30 @@
 					},
 					contentType: "application/json",
 					success: function(response_json_address) {
-						/*geocoder.geocode({'address': response_json_address.full_address}, function(result, status) {
-							if(response_json_address) {
-								var mylocation = {
-									lat: response_json_address.longitude,
-									lng: response_json_address.latitude
-								};
+						var full_address = response_json_address.full_address;
+						//console.log(full_address);
 
-								map.setCenter(response_json_address.longitude, response_json_address.latitude);
+						if(full_address == undefined) {
+							console.log(response_json_address.type_error);
+						} else {					
+							geocoder.geocode({'address': full_address}, function(result, status) {
+								if(status == 'OK') {
+									map.setCenter(result[0].geometry.location);
 
-								var marker = new google.maps.Marker({
-									map: map,
-									position: mylocation 
-								});
-							} else {
-								console.log('error');
-							}
-						});*/
-
-						geocoder.geocode({'address': response_json_address.full_address}, function(result, status) {
-							if(status == 'OK') {
-								map.setCenter(result[0].geometry.location);
-
-								var marker = new google.maps.Marker({
-									map: map,
-									position: result[0].geometry.location 
-								});
-							} else {
-								console.log(status);
-							}
-						});
+									var marker = new google.maps.Marker({
+										map: map,
+										position: result[0].geometry.location 
+									});
+								} else {
+									console.log(status);
+								}
+							});
+						}
 					}
 				});
 			});
-			
-
-			/*var address = '台北車站';
-
-			geocoder.geocode({'address': address}, function(result, status) {
-				if(status == 'OK') {
-					map.setCenter(result[0].geometry.location);
-					var marker = new google.maps.Marker({
-						map: map,
-						position: result[0].geometry.location
-					});
-				} else {
-					console.log(status);
-				}
-			});*/
 		}
-	</script-->
+	</script>
 
 	<!-- Ajax dropdown list -->
 	<script>
@@ -217,7 +188,7 @@
 		});
 	</script>
 
-	<!--script async defer src="https://maps.googleapis.com/maps/api/js?key=Your_API_Key&callback=initMap"></script-->
+	<script async defer src="https://maps.googleapis.com/maps/api/js?key=Your_API_KEY&callback=initMap"></script>
 </body>
 
 </html>
